c8cbdce2bf07a3f47a3d9164e4dc5a2c3afbf65c
Karzack <bj88svensson@gmail.com>
2016-03-14 16:32:23 +0100

    Controller
    Controller commit

--- WorkSpace/Chat/src/p3/ClientHandler.java
+++ WorkSpace/Chat/src/p3/ClientHandler.java
@@ -13,37 +13,8 @@ import java.util.Observer;
  *
  */
 public class ClientHandler extends Thread implements Observer{
-	private Socket socket;
-	private Message message;
-	private Message lastMessage;
 
 	public ClientHandler(Socket socket){
-		this.socket = socket;
-	}
-
-	public void run() {
-		System.out.println("Klient ansluten");
-		try(ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream())){
-			while(true){
-				System.out.println(message + "ute");
-				if(message != null && message != lastMessage){
-					System.out.println(message + "inne");
-					oos.writeObject(message);
-					oos.flush();
-					lastMessage = message;
-				}
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		System.out.println("Klient DC");
-	}
-
-	@Override
-	public void update(Observable o, Object arg) {
-		if(arg instanceof Message){
-			message=(Message)arg;
-		}
-
+		
 	}
 }
--- WorkSpace/Chat/src/p3/ServerController.java
+++ WorkSpace/Chat/src/p3/ServerController.java
@@ -1,18 +1,49 @@
 package p3;
+
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
 /**
  * (Server)
  * Main klassen f�r programmet,hanterar hur det generella fl�det i programmet g�r.
  * @author Ludwig
  *
  */
-public class ServerController {
+public class ServerController extends Thread{
 	private Server server;
+	private Socket socket;
+	private Message message;
+	private Message lastMessage;
 	
-	public ServerController(String address, int port){
-		this.server = new Server(this,address, port);
+	public ServerController(int port){
+		this.server = new Server(this, port);
+		
 	}
-	
+		public void setSocket(Socket socket) {
+		this.socket = socket;
+	}
+		
+	public void run() {
+		System.out.println("Klient ansluten");
+		try(ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream())){
+			while(true){
+				System.out.println(message + "ute");
+				if(message != null && message != lastMessage){
+					System.out.println(message + "inne");
+					oos.writeObject(message);
+					oos.flush();
+					lastMessage = message;
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Klient DC");
+	}
 	public static void main(String[] args){
 		
 	}
+
+
 }
