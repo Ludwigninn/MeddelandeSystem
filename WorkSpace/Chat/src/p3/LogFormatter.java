3398737e97f9b4c91bed9a699a8812f9b8a43f0a
Karzack <bj88svensson@gmail.com>
2016-03-17 09:56:02 +0100

    log
    logFormatter update

--- WorkSpace/Chat/src/p3/LogFormatter.java
+++ WorkSpace/Chat/src/p3/LogFormatter.java
@@ -1,13 +1,17 @@
 package p3;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.logging.ConsoleHandler;
 import java.util.logging.FileHandler;
-import java.util.logging.Handler;
 import java.util.logging.Level;
-import java.util.logging.LogRecord;
 import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+import javax.xml.ws.LogicalMessage;
 
 import p3.Message.MessageType;
 
@@ -19,25 +23,24 @@ import p3.Message.MessageType;
  */
 public class LogFormatter {
 	private Logger log;
+	private FileHandler fh;
 
 	public LogFormatter() throws SecurityException, IOException{
 		log = Logger.getLogger("Chatlog");
-		log.addHandler(new FileHandler("Logfiles/"+log.getName()+"%u.txt",true));
+		fh = new FileHandler("Logfiles/"+log.getName()+" "+new SimpleDateFormat("M-d_HHmmss").format(Calendar.getInstance().getTime())+".txt",true);
+		log.addHandler(fh);
+		fh.setFormatter(new SimpleFormatter());
+
 	}
+
 	public void logMessage(Message message) {
-		LogRecord logRecord = new LogRecord(Level.INFO,  "\n"+message.getMessage());
-		log.info(logRecord.getMessage());
-		
+		try{
+			log.info(""+ message.getType() + ": "+ message.getMessage());
+		}catch(Exception e){ 
+			logError(e);}
 	}
-
-	public static void main(String[] args) throws SecurityException, IOException {
-		LogFormatter lf = new LogFormatter();
-		Message newMessage = new Message(MessageType.Command, "Hallå!");
-		
-		lf.logMessage(newMessage);
-		newMessage=new Message(MessageType.Group,"Tjena!");
-		lf.logMessage(newMessage);
+	public void logError(Exception e){
+		log.warning(""+e);
 	}
-
 }
 
